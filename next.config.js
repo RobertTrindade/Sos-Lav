74208cb2fca056fda9d1c76d20a8466c9902b831
Thiago Barbosa <thiagobarbosadealencar606@gmail.com>
2024-01-05 11:19:15 -0300

    feat: Ajuste na integracao com fast

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const generateNextConfig = () => {
-
-  console.log(process.env.NODE_ENV)
   return {
     reactStrictMode: true, // Enable React strict mode for improved error handling
     swcMinify: true, // Enable SWC minification for improved performance
@@ -14,13 +12,4 @@ const generateNextConfig = () => {
   };
 };
 
-// Configuration object tells the next-pwa plugin
-const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
-});
-
-// Export the combined configuration for Next.js with PWA support
-module.exports = withPWA(generateNextConfig());
+module.exports = generateNextConfig();
